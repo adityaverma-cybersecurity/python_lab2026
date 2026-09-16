60ca1ea8c4a3abad97a3da5c5e93a9fe705df682
Aditya Verma <adiverma1to5@gmail.com>
2026-09-16 11:43:25 +0530

    Update Python programs

--- duplicate_detetion.py
+++ duplicate_detetion.py
@@ -1,11 +1,23 @@
-print("enter number to make the list")
+
+print("Enter 5 numbers to make the list")
+
 a = []
+
 for i in range(5):
     num = int(input())
     a.append(num)
-duplicate=0
-for i in range(len(a)):
-    if (duplicate<0):
-        remove = a[i]
-print("the list is", a)
 
+i = 0
+
+while i < len(a):
+    j = i + 1
+
+    while j < len(a):
+        if a[i] == a[j]:
+            a.remove(a[j])
+        else:
+            j += 1
+
+    i += 1
+
+print("The list is:", a)
